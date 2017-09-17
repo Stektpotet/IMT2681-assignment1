1a4ea9ab35b707c8a2d7d3083f0654476cd3ec33
Halvor Bakken Smedås <stektpotet@gmail.com>
2017-09-17 17:44:02 +0200

    trying to get remote version working

--- main.go
+++ main.go
@@ -8,9 +8,6 @@ import (
 	"net/http"
 	"os"
 	"strings"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 const BASE_PATH = "/projectinfo/v1/"
@@ -135,14 +132,9 @@ func getRequestBody(repoPath string) []byte {
 func main() {
 	// client := github.NewClient(nil)
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-
-	router := gin.New()
-	router.Static("/static", "static")
-
-	router.GET(BASE_PATH, githubProjectInfoHandler)
-	router.Run(":" + port)
+	http.HandleFunc(BASE_PATH, githubProjectInfoHandler)
+	http.ListenAndServe(":"+port, nil)
 }
