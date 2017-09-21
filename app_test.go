7d00694e6c56e9f2867b3e00b1e8d7bdc69e2e91
Halvor Bakken Smedås <stektpotet@gmail.com>
2017-09-21 20:20:26 +0200

    Version one working

--- app_test.go
+++ app_test.go
@@ -3,19 +3,23 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 )
 
+func TestMain(m *testing.M) {
+
+	os.Exit(m.Run())
+}
+
 func Test_JSONUnmarshalProjectInfo(t *testing.T) {
 	const jsonBody = `{
-        "name": "Project1",
-        "owner": {
-            "login": "Bob",
-            "id": 12743
-        }
+        "full_name": "Bob/Project1",
+        "owner": {"login": "Bob"}
     }`
-	// own := GithubUser{Username: "Bob", ID: 12743}
-	expectedResult := ProjectInfo{Name: "Project1", Owner: GithubUser{Username: "Bob", ID: 12743}}
+	expectedResult := ProjectInfo{Name: "Bob/Project1", Owner: GithubUser{Username: "Bob"}}
 	var result ProjectInfo
 	if err := json.Unmarshal([]byte(jsonBody), &result); err != nil {
 		t.Error(err)
@@ -24,13 +28,12 @@ func Test_JSONUnmarshalProjectInfo(t *testing.T) {
 		log.Println(err)
 	}
 	if result != expectedResult {
-		t.Errorf("Result:\t%s\tnot matching expected:\t%s\n", result, expectedResult)
+		t.Errorf("Result:\t%+v\tnot matching expected:\t%+v\n", result, expectedResult)
 	}
 }
 
 func Test_JSONMarshallResponse(t *testing.T) {
 	const expectedResult = `{"project":"kafka","owner":"apache","committer":"enothereska","commits":19,"language":["Java","Scala","Python","Shell","Batchfile"]}`
-	// exp := strings.TrimSpace(expectedResult)
 	responseBody := ResponseData{
 		Project:   "kafka",
 		Owner:     "apache",
@@ -46,21 +49,89 @@ func Test_JSONMarshallResponse(t *testing.T) {
 	}
 }
 
-func Test_getRepoPathSuccess(t *testing.T) {
-	testPath := "/projectinfo/v1/github.com/Stektpotet/Amazeking"
-	expectedRepoPath := "repos/Stektpotet/Amazeking"
-	resultRepoPath := getRepoPath(testPath)
-	if resultRepoPath != expectedRepoPath {
-		t.Errorf("%s not matching expected %s", resultRepoPath, expectedRepoPath)
-	}
+//==============================GET REPO PATH TESTS===========================
 
+var GetRepoPathTest = func(in string, expectedOut string, expectError bool, t *testing.T) {
+	resultRepoPath, err := getRepoPath(in)
+	if resultRepoPath != expectedOut {
+		t.Errorf("\"%s\" not matching expected \"%s\"", resultRepoPath, expectedOut)
+	}
+	if expectError {
+		if err == nil {
+			t.Errorf("expected error because %s is invalid as path", in)
+		}
+	} else {
+		if err != nil {
+			t.Errorf("unexpected error occured %s! %s is invalid as path", err.Error(), in)
+		}
+	}
 }
 
-func Test_getRepoPathFail(t *testing.T) {
-	testPath := "/Stektpotet/Amazeking"
-	resultRepoPath := getRepoPath(testPath)
-	if resultRepoPath != DEFAULT_REPO_PATH {
-		t.Errorf("%s not matching defaulting path %s", resultRepoPath, DEFAULT_REPO_PATH)
-	}
+func Test_GetRepoPathDefault(t *testing.T) {
+	GetRepoPathTest(ServiceBasePath+DefaultRepoPath, DefaultRepoPath, false, t)
+}
 
+func Test_GetRepoPathEmpty(t *testing.T) {
+	GetRepoPathTest("", DefaultRepoPath, true, t)
+}
+
+func Test_GetRepoPathNonValid(t *testing.T) {
+	GetRepoPathTest("cov/fe/fe", DefaultRepoPath, true, t)
+}
+
+//==============================SERVICE HANDLER REQUEST TESTS =================
+
+func Test_ServiceHandlerSEND(t *testing.T) {
+	reqest, _ := http.NewRequest("SEND", ServiceBasePath+DefaultRepoPath, nil)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(serviceHandler)
+	handler.ServeHTTP(rr, reqest)
+
+	if status := rr.Code; status != http.StatusMethodNotAllowed {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func Test_ServiceHandlerGET(t *testing.T) {
+	reqest, _ := http.NewRequest("GET", ServiceBasePath+DefaultRepoPath, nil)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(serviceHandler)
+	handler.ServeHTTP(rr, reqest)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+//=================================FULL SERVICE TESTS==========================
+
+var FullServiceTest = func(path string, devEnv bool, expectError bool, t *testing.T) {
+	serviceResponse, err := runService(path, devEnv)
+	if expectError {
+		if err == nil {
+			t.Errorf("Error did not occur when error was expected!\n response is instead %+v", serviceResponse)
+		}
+	} else {
+		if err != nil {
+			t.Errorf("unexpected error occured \"%s\" when running service.", err.Error())
+		}
+		if serviceResponse.Project == "" {
+			t.Errorf("service returns project with no name!\n%+v", serviceResponse)
+		}
+		if serviceResponse.Owner == "" {
+			t.Errorf("service returns project wit no owner!\n%+v", serviceResponse)
+		}
+	}
+}
+
+func Test_RunServiceRemote(t *testing.T) {
+	FullServiceTest(ServiceBasePath+DefaultRepoPath, true, false, t)
+}
+
+func Test_RunServiceLocal(t *testing.T) {
+	FullServiceTest(ServiceBasePath+DefaultRepoPath, true, false, t)
+}
+func Test_RunServiceLocalBadRequest(t *testing.T) {
+	FullServiceTest("", true, true, t)
 }
--- main.go
+++ main.go
@@ -2,30 +2,37 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
-const BASE_PATH = "/projectinfo/v1/"
-const DEFAULT_REPO_PATH = "repos/apache/kafka"
+//ServiceBasePath -Base path for this service
+const ServiceBasePath = "/projectinfo/v1/"
 
-// GITHUB API HOST URL for access to github api
-const GITHUB_API_HOST_URL = "https://api.github.com/"
+//DefaultRepoPath - Default path to handle if no/too few parameters are given
+const DefaultRepoPath = "repos/apache/kafka"
 
+//GithubAPIHostURL - for access to github api
+const GithubAPIHostURL = "https://api.github.com/"
+
+//ProjectInfo - The project name and its owner
 type ProjectInfo struct {
-	Name  string     `json:"name"`
+	Name  string     `json:"full_name"`
 	Owner GithubUser `json:"owner"`
 }
+
+//GithubUser - Holder for username and commits given
 type GithubUser struct {
 	Username string `json:"login"`
-	ID       uint   `json:"id"`
 	Commits  uint16 `json:"contributions"`
 }
 
+//ResponseData - the data returned by this service
 type ResponseData struct {
 	Project   string   `json:"project"`
 	Owner     string   `json:"owner"`
@@ -34,93 +41,99 @@ type ResponseData struct {
 	Languages []string `json:"language"`
 }
 
-func githubProjectInfoHandler(writer http.ResponseWriter, r *http.Request) {
-
-	//TODO - Only handle r.Method == "GET"
-	writer.Header().Add("content-type", "application/json")
-	var serviceResponse ResponseData
-
-	// fmt.Fprintf(writer "user: %s\n", pathVars[4])
-	// fmt.Fprintf(writer "repo: %s\n", pathVars[5])
-	repoPath := getRepoPath(r.URL.Path)
-	contributorsPath := repoPath + "/contributors"
-	languagesPath := repoPath + "/languages"
-
-	var project ProjectInfo
-	jsonBody := getRequestBody(repoPath)
-
-	if err := json.Unmarshal(jsonBody, &project); err != nil {
-		log.Println(string(jsonBody))
-		log.Println(err)
-	}
-	fmt.Fprintln(writer, "=============================\tProject info\t=============================")
-	fmt.Fprintln(writer, "project name:\t"+project.Name)
-	fmt.Fprintln(writer, "owner name:\t"+project.Owner.Username)
-	serviceResponse.Project = project.Name
-	serviceResponse.Owner = project.Owner.Username
-
-	topContributor := GithubUser{Username: "", ID: 0, Commits: 0}
-	fmt.Fprintln(writer, "=============================\tContributors\t=============================")
-	jsonBody = getRequestBody(contributorsPath)
-	contributors := make([]GithubUser, 0) //https://coderwall.com/p/4c2zig/decode-top-level-json-array-into-a-slice-of-structs-in-golang
-	if err := json.Unmarshal(jsonBody, &contributors); err != nil {
-		log.Println(string(jsonBody))
-		log.Println(err)
-	}
-	for i := 0; i < len(contributors); i++ {
-		fmt.Fprintf(writer, "Contributor:\t%s, commits: %v\n", contributors[i].Username, contributors[i].Commits)
-		if contributors[i].Commits > topContributor.Commits {
-			topContributor = contributors[i]
-		}
-	}
-	fmt.Fprintf(writer, "\nTop Contributor:\t%s, who made %v commits\n", topContributor.Username, topContributor.Commits)
-
-	serviceResponse.Committer = topContributor.Username
-	serviceResponse.Commits = topContributor.Commits
-
-	fmt.Fprintln(writer, "=============================\tLanguages\t=============================")
-	jsonBody = getRequestBody(languagesPath)
-
-	var topLanguage string
-	topLanguageBytes := 0
-	languageMap := make(map[string]int) //https://coderwall.com/p/4c2zig/decode-top-level-json-array-into-a-slice-of-structs-in-golang
-	if err := json.Unmarshal(jsonBody, &languageMap); err != nil {
-		log.Println(string(jsonBody))
-		log.Println(err)
-	}
-	languages := make([]string, 0, len(languageMap))
-	for key, value := range languageMap {
-		fmt.Fprintf(writer, "Language:\t%s,\t bytes of code: %v\n", key, value)
-		languages = append(languages, key)
-		if topLanguageBytes < value {
-			topLanguage = key
-			topLanguageBytes = value
-		}
-	}
-	fmt.Fprintf(writer, "\nMost used Language:\t%s, with %v bytes of code\n", topLanguage, topLanguageBytes)
-
-	serviceResponse.Languages = languages
-	json.NewEncoder(writer).Encode(serviceResponse)
-
-	// fmt.Fprintln(writer "Projectinfo: "+string(getRequestBody(repoPath)))
-
+//RequestBodies - local json bodies for the three requests that would be done when deployed
+type RequestBodies struct {
+	Project      []byte
+	Contributors []byte
+	Languages    []byte
 }
 
-func getRepoPath(originalPath string) string {
-	//projectinfo/v1/repos/stektpotet/Amazeking
-	URLPath := originalPath
-
-	pathVars := strings.Split(URLPath, "/")
-	if len(pathVars) < 5 {
-		URLPath = DEFAULT_REPO_PATH
+func getProjectInfo(jsonBody []byte, r *ResponseData) {
+	var project ProjectInfo
+	if err := json.Unmarshal(jsonBody, &project); err != nil {
+		log.Fatalln(string(jsonBody))
+		log.Fatalln(err)
 	}
+	r.Project = "github.com/" + project.Name
+	r.Owner = project.Owner.Username
+}
 
-	URLPath = strings.TrimPrefix(URLPath, BASE_PATH)
-	return strings.Replace(URLPath, "github.com", "repos", 1)
+func getContributorInfo(jsonBody []byte, r *ResponseData) {
+	contributors := make([]GithubUser, 0)
+	if err := json.Unmarshal(jsonBody, &contributors); err != nil {
+		log.Fatalln(string(jsonBody))
+		log.Fatalln(err)
+	}
+	r.Committer = contributors[0].Username
+	r.Commits = contributors[0].Commits
+}
+
+func getLanguageInfo(jsonBody []byte, r *ResponseData) {
+
+	languageMap := make(map[string]int) //https://coderwall.com/p/4c2zig/decode-top-level-json-array-into-a-slice-of-structs-in-golang
+	if err := json.Unmarshal(jsonBody, &languageMap); err != nil {
+		log.Fatalln(string(jsonBody))
+		log.Fatalln(err)
+	}
+	languages := make([]string, 0, len(languageMap))
+	for key := range languageMap {
+		languages = append(languages, key)
+	}
+	r.Languages = languages
+}
+
+func runService(path string, devEnv bool) (ResponseData, error) {
+	var serviceResponse ResponseData
+	var jsonBodies RequestBodies
+	if devEnv {
+		//this next line is only to test if a given path is valid
+		_, serviceError := getRepoPath(path)
+		if serviceError != nil {
+			return ResponseData{}, serviceError
+		}
+		jsonBodies = setupLocal() //local service only uses DefaultRepoPath
+	} else {
+		log.Println("runing remotely!")
+		repoPath, serviceError := getRepoPath(path)
+		if serviceError != nil {
+			return ResponseData{}, serviceError
+		}
+		jsonBodies = setupRemote(repoPath)
+	}
+	getProjectInfo(jsonBodies.Project, &serviceResponse)
+	getContributorInfo(jsonBodies.Contributors, &serviceResponse)
+	getLanguageInfo(jsonBodies.Languages, &serviceResponse)
+	return serviceResponse, nil
+}
+
+func serviceHandler(writer http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		devEnv, _ := strconv.ParseBool(os.Getenv("DEVENV"))
+		writer.Header().Add("content-type", "application/json")
+		serviceResponse, err := runService(r.URL.Path, devEnv)
+		if err != nil {
+			http.Error(writer, http.StatusText(http.StatusBadRequest)+"\n"+err.Error(), http.StatusBadRequest)
+		}
+		json.NewEncoder(writer).Encode(serviceResponse)
+	} else {
+		http.Error(writer, "Only GET requests allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func getRepoPath(originalPath string) (string, error) {
+	URLPath := originalPath
+	pathVars := strings.Split(URLPath, "/")
+	if len(pathVars) != 6 {
+		return DefaultRepoPath, errors.New("Invalid URL PATH: " + URLPath)
+	}
+	//@TODO regex filtering username
+
+	URLPath = strings.TrimPrefix(URLPath, ServiceBasePath)
+	return strings.Replace(URLPath, "github.com", "repos", 1), nil
 }
 
 func getRequestBody(repoPath string) []byte {
-	response, err := http.Get(GITHUB_API_HOST_URL + repoPath)
+	response, err := http.Get(GithubAPIHostURL + repoPath)
 	if err != nil {
 
 	}
@@ -132,12 +145,39 @@ func getRequestBody(repoPath string) []byte {
 	return body
 }
 
+func setupLocal() RequestBodies {
+	var requestBodies RequestBodies
+
+	requestBodies.Project = readLocalWorkingFile("project")
+	requestBodies.Languages = readLocalWorkingFile("languages")
+	requestBodies.Contributors = readLocalWorkingFile("contributors")
+
+	return requestBodies
+}
+
+func setupRemote(repoPath string) RequestBodies {
+	return RequestBodies{
+		Project:      getRequestBody(repoPath),
+		Languages:    getRequestBody(repoPath + "/languages"),
+		Contributors: getRequestBody(repoPath + "/contributors"),
+	}
+}
+
+func readLocalWorkingFile(filename string) []byte {
+	devPath := os.Getenv("LOCALPROJECT")
+	data, err := ioutil.ReadFile(devPath + filename + ".json")
+	if err != nil {
+		log.Fatalf("Could not open local working file:%s", devPath+filename+".json")
+	}
+	return data
+}
+
 func main() {
-	// client := github.NewClient(nil)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-	http.HandleFunc(BASE_PATH, githubProjectInfoHandler)
+	http.HandleFunc(ServiceBasePath, serviceHandler)
 	http.ListenAndServe(":"+port, nil)
 }
